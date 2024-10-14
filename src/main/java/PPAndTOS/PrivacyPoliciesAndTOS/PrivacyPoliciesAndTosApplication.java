92f7b1c2f46c5e89d7fdff4dc7eb4921acd91a35
SaiKumar Mojjada <saimojjada1234@gmail.com>
2024-10-14 17:37:23 -0400

    First commit

---
+++ src/main/java/PPAndTOS/PrivacyPoliciesAndTOS/PrivacyPoliciesAndTosApplication.java
@@ -0,0 +1,13 @@
+package PPAndTOS.PrivacyPoliciesAndTOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyPoliciesAndTosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyPoliciesAndTosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PPAndTOS/PrivacyPoliciesAndTOS/PrivacyPoliciesAndTosApplicationTests.java
@@ -0,0 +1,13 @@
+package PPAndTOS.PrivacyPoliciesAndTOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacyPoliciesAndTosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
